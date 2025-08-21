2db4c992305b83d4416af439c88f6129dbca0978
kalvin <jhonkalvinporteria@gmail.com>
2025-08-21 20:15:15 +0800

    updated dependencies and but unfinished authentication

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
 }
 
 android {
+    ndkVersion = "27.0.12077973"
+
     namespace = "com.example.app_revamp_5"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
@@ -21,11 +23,10 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.app_revamp_5"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        
+        // ✅ Use Kotlin DSL style, no "minSdkVersion 23"
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -34,7 +35,6 @@ android {
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
